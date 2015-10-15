7309e828ec4778f59419e202685fcc9eaab27b8c
guoyp03@gmail.com <guoyp03@gmail.com>
2015-10-15 14:11:03 +0800

    setup

---
+++ mstats/mstats.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+
+import requests
+
+urls = ['http://qt.gtimg.cn/q=bkqtRank_A_sh',
+'http://qt.gtimg.cn/q=bkqtRank_A_sz']
+
+
+def getStatsInfo(url):
+    try:
+        rest = requests.get(url)
+        rst = rest.text
+        infos = rst.split('=')[1].replace('"', '').split('~')
+        up = int(infos[2])
+        sideway = int(infos[3])
+        down = int(infos[4])
+        print (up,sideway,down,(1*(up)+0*(sideway)+(-1)*(down))/(float(up)+float(sideway)+float(down)))
+    except Exception as e:
+        print "Error:[%s]" % e
+
+def main():
+    for url in urls:
+        getStatsInfo(url)
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+from setuptools import setup, find_packages
+
+setup(
+    name='mstats',
+    version = '1.0.0',
+    description= 'show the basic statis of the market',
+    author = 'william',
+    author_email = 'guoyp03@gmail.com',
+    url = 'https://github.com/YuepengGuo',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'stats = mstats.mstats:main',
+        ]
+    },
+    install_requires=[
+        'requests',
+    ]
+)
